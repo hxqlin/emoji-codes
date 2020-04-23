130c67d2f95c8c4021a1d4fbbdc70dbc2b16b3c8
Hannah Lin <hxqlin@gmail.com>
2020-04-22 17:28:19 -0700

    #10 Attach emoji picker if the Messenger conversation is changed

--- src/content.jsx
+++ src/content.jsx
@@ -3,19 +3,43 @@ import ReactDOM from "react-dom";
 import EmojiPicker from "./EmojiPicker";
 import "./App.less";
 
-const setupEditorTracker = () => {
-  const chat = document.querySelector('div[role="main"]');
-  chat.style.position = "relative";
+/*
+ * Injects an emoji picker into the current conversation.
+ */
+const injectEmojiPicker = () => {
+  setTimeout(() => {
+    const chat = document.querySelector('div[role="main"]');
+    chat.style.position = "relative";
 
-  const emojiContainer = document.createElement("div");
-  emojiContainer.classList.add("emoji-container");
+    const emojiContainer = document.createElement("div");
+    emojiContainer.classList.add("emoji-container");
 
-  const editor = document.querySelector(
-    "div[contenteditable=true][role=combobox]"
-  );
+    const editor = document.querySelector(
+      "div[contenteditable=true][role=combobox]"
+    );
 
-  chat.appendChild(emojiContainer);
-  ReactDOM.render(<EmojiPicker chat={chat} editor={editor} />, emojiContainer);
+    chat.appendChild(emojiContainer);
+    ReactDOM.render(
+      <EmojiPicker chat={chat} editor={editor} />,
+      emojiContainer
+    );
+  }, 0);
 };
 
-window.addEventListener("load", setupEditorTracker);
+/*
+ * Set up an emoji picker for the current conversation and
+ * attach listener for changes to the conversation.
+ */
+const setupEmojiPicker = () => {
+  injectEmojiPicker();
+
+  /*
+   * When a different conversation is clicked, inject an emoji picker
+   * for the selected conversation.
+   */
+  const conversations = document.querySelector('div[role="main"]')
+    .previousSibling;
+  conversations.addEventListener("click", injectEmojiPicker);
+};
+
+window.addEventListener("load", setupEmojiPicker);
